546b56ebb7088c7dc89be0e5f7f9609f8c16178f
Tyler Perron <tperron31313@gmail.com>
2022-10-25 17:49:01 -0400

    first commit

---
+++ React Assignment/First_JSX(C)/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TestComponent from './components/FunctionalComponent/TestComponent'
+function App() {
+  return (
+    <div className="App">
+      {/* insert code here */}
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+      <TestComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ React Assignment/First_JSX(C)/src/components/FunctionalComponent/TestComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          *Learn React
+        </li>
+        <li>
+          *Create a dope project
+        </li>
+        <li>
+          *stuff and things
+        </li>
+        <li>
+          *Learn to save the world
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default TestComponent
