030ad2fa202eaa7c08de4ffc61170f53ac8180b6
Tyler Perron <tperron31313@gmail.com>
2022-10-27 15:32:01 -0400

    finished!

--- React Assignment/Tabs(P)/src/components/Masterfile/buttons.jsx
+++ React Assignment/Tabs(P)/src/components/Masterfile/buttons.jsx
@@ -3,15 +3,24 @@ import Tab1 from './functionalComponents/Tab1'
 import Tab2 from './functionalComponents/Tab2'
 import Tab3 from './functionalComponents/Tab3'
 
-const tabs = ['tab1','tab2','tab3']
-    
+const tabs = ['Tab1','Tab2','Tab3']
 
 const Buttons = (props) => {
-    const [active, setActive] = useState(tabs[0])
+    const [active, setActive] = useState('')
     console.log(active);
-    // const tabElement1 = () => { active === tabs[0] ? <Tab1/> : console.log("nope"); }
-    // const tabElement2 = () => { active === tabs[1] ? <Tab2/> : console.log("nope") }
-    // const tabElement3 = () => { active === tabs[2] ? <Tab3/> : console.log("nope") }
+    
+
+const activateTab = (e) => {
+        setActive(e.target.name)
+    }
+let activeDisplay = ''
+if (active === tabs[2]){
+    activeDisplay = <Tab3/>
+}else if (active === tabs[1]){
+    activeDisplay = <Tab2/>
+} else if (active === tabs[0]) {
+    activeDisplay = <Tab1/>
+}
 return (
     <>
         <div>
@@ -19,16 +28,14 @@ return (
                 <button
                 key={ tab }
                 name={ tab }
-                onClick={() => setActive(tab)}
+                onClick={activateTab}
                 > 
                 { tab }
                 </button>
             ))}
             
         </div>
-        { tabElement1 }
-        { tabElement2 }
-        { tabElement3 }
+                { activeDisplay }
     </>
 )
 }
